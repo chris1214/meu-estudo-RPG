4d376db4ce8cc76823f3edb0ee43f810f287faf0
Christopher andrio <christopher_andrio@hotmail.com>
2018-05-15 14:05:28 -0300

    Push

--- myRPG/src/components/create.vue
+++ myRPG/src/components/create.vue
@@ -3,6 +3,7 @@ export default{
     data(){
         return{
           dados: {
+          httpUsers: 'http://localhost:3000/users',
             desejoMeTornar: [
               {
                 label: 'Mestre',
@@ -33,32 +34,122 @@ export default{
             ],
           },
           resultados: {
-            email: { required: true, message: 'E-mail é obrigatório', trigger: 'blur' },
-            user: '',
+            name: '',
+            email: '',
             password: '',
-            confirmePassowrd: '',
             dataNascimento: '',
-            desejo: '',
-            experiencia: '',
-            experienciaDescricao: '',
+            nivelDeMestragem: 0,
+            nivelDePlayer: 0,
+            tags: {
+              desejo: '',
+              experiencia: '',
+              user: '',
+              nivel: ''
+            },
             experienciaDescricao: '',
           },
+          confirmePassowrd: '',
+          regra: {
+            email: [{ required: false, type: 'email', message: 'E-mail é obrigatório' }],
+            user: [{ required: false, message: 'Nome de Usuário é obrigatório', trigger: 'change' }],
+            password: [{ required: false, message: 'Senha é obrigatório', trigger: 'blur' }],
+            confirmePassowrd: [{ required: false, message: 'Confirmação de senha é obrigatório', trigger: 'blur' }],
+            dataNascimento: [{ required: false, message: 'Data de Nascimento é obrigatório', trigger: 'change' }],
+            desejo: [{ required: false, message: 'E-mail é obrigatório', trigger: 'change' }],
+            experiencia: [{ required: false, message: 'E-mail é obrigatório', trigger: 'change' }],
+          }
         }
     },
     components:{
     },
     methods: {
+      pushAll(){
+        this.$http.post(`http://localhost:3000/users/`, this.resultados).then(
+          response => {
+            this.$router.push('/')
+          }, error => {
+            console.log("Error");
+          }
+        )
+      },
       create(){
-        let email = this.resultados.email,
-        let user = this.resultados.user,
-        let password = this.resultados.password,
-        let confirmePassowrd = this.resultados.confirmePassowrd,
-        let dataNascimento = this.resultados.dataNascimento,
-        let desejo = this.resultados.desejo,
-        let experienciaDescricao = this.resultados.experienciaDescricao,
+        let email = this.resultados.email;
+        let name = this.resultados.name;
+        let password = this.resultados.password;
+        let confirmePassowrd = this.confirmePassowrd;
+        let dataNascimento = this.resultados.dataNascimento;
+        let desejo = this.resultados.tags.desejo;
+        let experiencia = this.resultados.tags.experiencia;
+        let experienciaDescricao = this.resultados.experienciaDescricao;
+
+
+        if (desejo == 'mestre'){
+
+          this.resultados.tags.user = 'Mestre';
+
+        }else if (desejo == 'player'){
+
+          this.resultados.tags.user = 'Player';
+
+        }else if (desejo == 'ambos'){
+
+          this.resultados.tags.user = 'Ambos';
+
+        }
+
+        if(this.resultados.tags.user == 'Mestre'){
+          if (experiencia == 'alta' || experiencia == 'media'){
+
+            this.resultados.nivelDeMestragem = 1;
+            this.resultados.tags.nivel = 'Mestre';
+
+          }else if (experiencia == 'baixa'){
+
+            this.resultados.nivelDeMestragem = 1;
+            this.resultados.tags.nivel = 'Aspirante a Mestre Aprendiz';
+
+          }
+        }
+
+        if(this.resultados.tags.user == 'Player'){
+          if (experiencia == 'alta' || experiencia == 'media'){
+
+            this.resultados.nivelDePlayer = 1;
+            this.resultados.tags.nivel = 'Player';
+
+          }else if (experiencia == 'baixa'){
+
+            this.resultados.nivelDePlayer = 1;
+            this.resultados.tags.nivel = 'Player Iniciante';
+
+          }
+        }
+
+        if(this.resultados.tags.user == 'Ambos'){
+          if (experiencia == 'alta' || experiencia == 'media'){
+
+            this.resultados.nivelDeMestragem = 1;
+            this.resultados.nivelDePlayer = 1;
+            this.resultados.tags.nivel = 'Player e Mestre';
+
+          }else if (experiencia == 'baixa'){
+
+            this.resultados.nivelDeMestragem = 1;
+            this.resultados.nivelDePlayer = 1;
+            this.resultados.tags.nivel = 'Player e Mestre Iniciante';
+
+          }
+        }
+        if (email && name && password && confirmePassowrd == password && dataNascimento && desejo && experiencia){
+          console.log("True")
+          this.pushAll();
+        }else{
+          console.log("OPS")
+        }
       }
     }
 }
+
 </script>
 <template>
   <div>
@@ -67,33 +158,34 @@ export default{
         <b-col md="8" offset-md="2">
           <b-card title="Create"
                   class="mb-2">
+            {{resultados.email}}
             <div>
-              <el-form label-position="top">
+              <el-form label-position="top" :rules="regra">
                 <el-row :gutter="20">
                   <el-col :span="12">
                     <el-form-item label="Email:">
-                      <el-input v-model="resultados.emal"></el-input>
+                      <el-input type="email" v-model="resultados.email"></el-input>
                     </el-form-item>
                   </el-col>
                   <el-col :span="12">
-                    <el-form-item label="Nome de usuário:">
-                      <el-input v-model="resultados.user"></el-input>
+                    <el-form-item label="Nome de usuário:" prop="user">
+                      <el-input v-model="resultados.name" type="text"></el-input>
                     </el-form-item>
                   </el-col>
                 </el-row>
                 <el-row :gutter="20">
                   <el-col :span="8">
-                    <el-form-item label="Senha:">
+                    <el-form-item label="Senha:" prop="password">
                       <el-input v-model="resultados.password" type="password"></el-input>
                     </el-form-item>
                   </el-col>
                   <el-col :span="8">
-                    <el-form-item label="Confirmação de Senha:">
-                      <el-input v-model="resultados.confirmePassowrd" type="password"></el-input>
+                    <el-form-item label="Confirmação de Senha:" prop="confirmePassowrd">
+                      <el-input v-model="confirmePassowrd" type="password"></el-input>
                     </el-form-item>
                   </el-col>
                   <el-col :span="8">
-                    <el-form-item label="Data de Nascimento:">
+                    <el-form-item label="Data de Nascimento:" prop="dataNascimento">
                       <div class="block">
                         <el-date-picker
                           v-model="resultados.dataNascimento"
@@ -106,8 +198,8 @@ export default{
 
                 <el-row :gutter="20">
                   <el-col :span="12">
-                    <el-form-item label="Desejo me tornar um:">
-                      <el-select v-model="resultados.desejo">
+                    <el-form-item label="Desejo me tornar um:" prop="desejo">
+                      <el-select v-model="resultados.tags.desejo">
                         <el-option
                           v-for="item in dados.desejoMeTornar"
                           :key="item.value"
@@ -118,8 +210,8 @@ export default{
                     </el-form-item>
                   </el-col>
                   <el-col :span="12">
-                    <el-form-item label="Minha experiencia com RPG é: ">
-                      <el-select v-model="resultados.experiencia">
+                    <el-form-item label="Minha experiencia com RPG é: " prop="experiencia">
+                      <el-select v-model="resultados.tags.experiencia">
                         <el-option
                           v-for="item in dados.minhaExperienciaComRpg"
                           :key="item.value"
@@ -129,8 +221,8 @@ export default{
                       </el-select>
                     </el-form-item>
                   </el-col>
-                  </el-row>
-                  <el-row :gutter="20">
+                </el-row>
+                <el-row :gutter="20">
                   <el-col :span="24">
                     <el-form-item label="Sua Experiencia com RPG">
                       <el-input
@@ -145,11 +237,9 @@ export default{
                 <el-form-item>
                   <el-row>
                     <el-col :span="12">
-                      <router-link to="/">
-                        <el-button type="primary" size="mini">
-                          Cadastre-se
-                        </el-button>
-                      </router-link>
+                      <el-button @click="create()" type="primary" size="mini">
+                        Cadastre-se
+                      </el-button>
                     </el-col>
                     <el-col class="align-right" :span="12">
                       <router-link to="/">
@@ -172,4 +262,5 @@ export default{
 .card-title {
    text-align: center;
 }
+
 </style>
--- myRPG/src/components/list.vue
+++ myRPG/src/components/list.vue
@@ -1,17 +1,75 @@
 <script>
-    export default{
-        data(){
-            return{
-            }
-        },
-        components:{
+export default{
+    data(){
+        return{
+           activeIndex: '2',
         }
-    }
+    },
+    components:{
+    },
+}
+
 </script>
 <template>
   <div>
-    <h1>list</h1>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+      <el-menu-item class="myDisableImg" disabled index="1">
+        <div class="containerIMG">
+          <img class="img" src="../img/Runa-Ansuz.png">
+        </div>
+      </el-menu-item>
+      <el-menu-item index="2">Procurar por mesa</el-menu-item>
+      <el-menu-item index="3">Criar Mesa</el-menu-item>
+      <el-menu-item index="4">Minhas mesas</el-menu-item>
+      <el-menu-item index="5">Mesas que já participo</el-menu-item>
+      <el-menu-item class="pullRight" index="7">Perfil</el-menu-item>
+      <span style="margin-top: 11px;" class="pullRight">
+        <el-input
+          placeholder="Filtro"
+          prefix-icon="el-icon-search">
+        </el-input>
+      </span>
+    </el-menu>
+    <div class="background-IMG">
+      <b-container fluid class="padding-top">
+        <b-row>
+          <b-col>col</b-col>
+          <b-col>col</b-col>
+          <b-col>col</b-col>
+          <b-col>col</b-col>
+          <b-col>col</b-col>
+          <b-col>col</b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 <style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+.pullRight {
+  float: right !important;
+}
+.containerIMG {
+  max-height: 60px;
+  max-width: 60px;
+  height: 60px;
+}
+.img {
+  height: 100%;
+}
+.myDisableImg {
+  opacity: 1 !important;
+}
+
+.padding-top {
+  padding-top: 50px
+}
+.background-IMG {
+  background-image: url('../img/kingCastle.jpg');
+  height: 100vh;
+}
+
 </style>
