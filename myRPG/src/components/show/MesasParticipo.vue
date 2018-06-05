b26450784eb826500d9ff982c44017b79678b0ec
chris1214 <christopher_andrio@hotmail.com>
2018-06-05 14:03:09 -0300

    Minhas mesas

--- myRPG/src/components/show/MesasParticipo.vue
+++ myRPG/src/components/show/MesasParticipo.vue
@@ -4,6 +4,7 @@ export default{
   props: ['id', 'name'],
   data(){
       return{
+        minhaMesa: [],
         mesas: [],
         myPlayers: [],
         httpPlayers: 'http://localhost:3000/players',
@@ -14,7 +15,7 @@ export default{
     renderImagem
   },
   methods: {
-    getAll(myHttp, resposta, id){
+    /*getAll(myHttp, resposta, id){
       this.$http.get(`${myHttp}${id}`).then(
         response => {
           this[resposta] = response.body;
@@ -38,10 +39,33 @@ export default{
           }
         }
       )
-    },
+    },*/
     myGet(){
-      this.getAll(`${this.httpPlayers}`, 'myPlayers', `?userId=${this.id}`)
-        /*this.getAll(`${this.httpMesas}`, 'mesas', `?id=${}`)*/
+      this.$http.get(`${this.httpPlayers}?userId=${this.id}`).then(
+        response => {
+          this.myPlayers = response.body;
+        }, error => {
+          console.log("Error")
+        }
+      ).then(
+        function () {
+          var mesas = this.myPlayers.length;
+          console.log(mesas);
+          for (var i = 0; i < mesas; i++){
+            console.log(this.myPlayers[i].mesaId);
+            this.$http.get(`${this.httpMesas}?id=${this.myPlayers[i].mesaId}`).then(
+              response => {
+                this.mesa = response.body;
+                this.minhaMesa.push( response.body);
+                console.log(this.minhaMesa)
+              }, error => {
+                console.log("Error")
+              }
+            )
+          }
+        }
+      );
+      console.log(this.mesa)
     }
   },
   created(){
@@ -53,10 +77,9 @@ export default{
   <div>
     <b-container fluid class="padding-top">
       <el-row :gutter="20">
-        <p v-for="item in mesas">{{item}}</p><br><br>
-        <br><br>
+        <p v-for="item in minhaMesa">{{item}}</p><br><br>
         <p v-for="item in myPlayers">{{item}}</p><br><br>
-        <div v-for="mesa in mesas">
+        <div v-for="mesa in minhaMesa">
           <el-col :xs="12" :sm="6" :md="6" :lg="4" :xl="4" @click="enter()">
             <renderImagem
               :id="id"
